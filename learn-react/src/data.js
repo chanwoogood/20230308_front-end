be16df21d3afcba91030abf2175ab701805366bc
chanwoogood <127277342+chanwoogood@users.noreply.github.com>
2023-03-28 10:38:40 +0900

    9. 이벤트 처리

--- learn-react/src/data.js
+++ learn-react/src/data.js
@@ -1,17 +1,32 @@
-import { people } from "./data.js";
-import { getImageUrl } from "./utils.js";
-
-export default function List() {
-  const chemists = people.filter((person) => person.profession === "chemist");
-  const listItems = chemists.map((person) => (
-    <li>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {" " + person.profession + " "}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  ));
-  return <ul>{listItems}</ul>;
-}
+export const people = [{
+    id: 0, // Used in JSX as a key
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A'
+  }, {
+    id: 1, // Used in JSX as a key
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa'
+  }, {
+    id: 2, // Used in JSX as a key
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+    accomplishment: 'electromagnetism theory',
+    imageId: 'bE7W1ji'
+  }, {
+    id: 3, // Used in JSX as a key
+    name: 'Percy Lavon Julian',
+    profession: 'chemist',
+    accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+    imageId: 'IOjWm71'
+  }, {
+    id: 4, // Used in JSX as a key
+    name: 'Subrahmanyan Chandrasekhar',
+    profession: 'astrophysicist',
+    accomplishment: 'white dwarf star mass calculations',
+    imageId: 'lrWQx8l'
+  }];
+  
