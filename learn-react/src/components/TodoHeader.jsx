a8f18055e0a036febde3b54260d45efae92076c7
chanwoogood <127277342+chanwoogood@users.noreply.github.com>
2023-03-29 11:53:13 +0900

    15. 컴포넌트 분리해서 작성하기

---
+++ learn-react/src/components/TodoHeader.jsx
@@ -0,0 +1,14 @@
+export default function TodoHeader({ todos }) {
+  const dateStr = new Date().toLocaleDateString("ko-KR", { dateStyle: "full" });
+
+  const undoneCount = todos.filter((todo) => !todo.done).length;
+
+  return (
+    <div>
+      <h2>{dateStr}</h2>
+      <p>
+        해야할 일 : {undoneCount}/{todos.length}
+      </p>
+    </div>
+  );
+}
