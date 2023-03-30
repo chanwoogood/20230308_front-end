8330ce31d83d0a5c185f675498c1bdc9d5818ef0
chanwoogood <127277342+chanwoogood@users.noreply.github.com>
2023-03-30 11:02:49 +0900

    20. Context.api 기본

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,21 +1,5 @@
-import { useReducer, todosReducer, get } from "react";
-import Counter from "./components/Counter";
-import TodoHeader from "./components/TodoHeader";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
-
-function getUndoneCount(todos) {
-  console.log("해야할 일 세는 중...");
-  return todos.filter((todo) => !todo.done).length;
-}
+import GrandParent from "./components/GrandParent";
 
 export default function App() {
-  const [todos, dispatch] = useReducer(todosReducer, []);
-  return (
-    <div>
-      <TodoHeader />
-      <TodoInput dispatch={dispatch} />
-      <TodoList todos={todos} dispatch={dispatch} />
-    </div>
-  );
+  return <GrandParent />;
 }
---
+++ learn-react/src/components/UserInfo.jsx
@@ -0,0 +1,24 @@
+import { useInputs } from "../hooks/useInputs";
+
+// src/components/UserInfo.jsx
+function UserInfo() {
+  const [inputs, handleInputs, reset] = useInputs({
+    name: "",
+    email: "",
+  });
+
+  const { name, email } = inputs;
+
+  return (
+    <div>
+      <h2>
+        {name}({email})
+      </h2>
+      <input type="text" name="name" onChange={handleInputs} />
+      <input type="text" name="email" onChange={handleInputs} />
+      <button onClick={reset}>초기화</button>
+    </div>
+  );
+}
+
+export default UserInfo;
